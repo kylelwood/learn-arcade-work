59dbd18710faa899e2e04cf7711679169bdd93b2
Kdub <klw2198@email.vccs.edu>
2023-03-22 22:27:53 -0400

    lab 7

--- Lab 07 - User Control/lab_07.py
+++ Lab 07 - User Control/lab_07.py
@@ -8,61 +8,56 @@ SCREEN_HEIGHT = 600
 GAME_TITLE = "FLYING COIN RACE"
 
 CHARACTER_SCALING = 1
-MOVEMENT_SPEED = 2
-DRIFT_SPEED = -.5
-
+MOVEMENT_SPEED = 2.5
 
 def load_texture_pair(filename):
-    return [arcade.load_texture(filename), arcade.load_texture(filename, flipped_horizontally=True)]
+    return [arcade.load_texture(filename), arcade.load_texture(filename, flipped_horizontally = True)]
 
 
-class FighterPlane1:
-    """First of 2 planes that will fly on screen"""
-    def __init__(self, position_x, position_y, change_x, change_y, size):
+class FighterPlane:
+    """2 planes that will fly on screen"""
+    def __init__(self, position_x, position_y, change_x, change_y, size, image):
+
         self.position_x = position_x
         self.position_y = position_y
         self.change_x = change_x
         self.change_y = change_y
         self.size = size
-
-        super().__init__()
+        self.image = image
 
         self.cur_texture = 0
         self.scale = CHARACTER_SCALING
 
-        main_path1 = ":resources:C:/Users/kylel/OneDrive/Computer Science/biplane-158327_1280.png"
-
-        self.fly_texture_pair = load_texture_pair(f"{main_path1}.png")
+        self.fly_texture_pair = load_texture_pair(self.image)
 
         self.texture = self.fly_texture_pair[0]
+
+
 
     def update(self):
         self.position_x += self.change_x
         self.position_y += self.change_y
 
-class FighterPlane2:
-    def __init__(self, position_x, position_y, change_x, change_y, size):
-        self.position_x = position_x
-        self.position_y = position_y
-        self.change_x = change_x
-        self.change_y = change_y
-        self.size = size
+        if self.position_x < self.size:
+            self.position_x = self.size
 
-        super().__init__()
+        if self.position_x > SCREEN_WIDTH - 50:
+            self.position_x = SCREEN_WIDTH - 50
 
-        self.cur_texture = 0
-        self.scale = CHARACTER_SCALING
+        if self.position_y < self.size:
+            self.position_y = self.size
 
-        main_path2 = ":resources:C:/Users/kylel/OneDrive/Computer Science/green-304704_640.png"
+        if self.position_y > SCREEN_HEIGHT - 50:
+            self.position_y = SCREEN_HEIGHT - 50
 
-        self.fly_texture_pair = load_texture_pair(f"{main_path2}.png")
+    def myupdate_animation(self):
+        if self.change_y > 0:
+            self.texture = self.fly_texture_pair[0]
+        elif self.change_y < 0:
+            self.texture = self.fly_texture_pair[1]
 
-        self.texture = self.fly_texture_pair[0]
-
-    def update(self):
-        self.position_x += self.change_x
-        self.position_y += self.change_y
-
+    def draw(self):
+        arcade.draw_lrwh_rectangle_textured(self.position_x, self.position_y, 55, 55, self.fly_texture_pair[0])
 
 
 class MyGame(arcade.Window):
@@ -76,16 +71,37 @@ class MyGame(arcade.Window):
 
         self.set_mouse_visible(False)
 
-        self.player1 = FighterPlane1(50, 50, 0, 0, 5)
-        self.player2 = FighterPlane2(50, 50, 0, 0, 5)
+        self.player1 = FighterPlane(700, 500, 0, 0, 5, "biplane-158327_1280.png")
+        self.player2 = FighterPlane(50, 500, 0, 0, 5, "green-304704_640.png")
+
+
 
     def on_draw(self):
         arcade.start_render()
+        arcade.set_background_color(arcade.color.BURNT_ORANGE)
+        self.draw_sun()
+        self.draw_sea()
+        self.draw_sailboat(150, 21)
         self.player1.draw()
         self.player2.draw()
 
     def update(self, delta_time):
         self.player1.update()
+        self.player2.update()
+
+    def draw_sun(self):
+        # draw the sun
+        arcade.draw_circle_filled(400, 165, 150, arcade.color.SUNRAY)
+
+    def draw_sea(self):
+        # draw the sea
+        arcade.draw_lrtb_rectangle_filled(0, 800, 70, 0, arcade.color.HONOLULU_BLUE)
+
+    def draw_sailboat(self, x, y):
+        # draw a sailboat
+        arcade.draw_lrtb_rectangle_filled(380 + x, 420 + x, 60 + y, 50 + y, arcade.color.BLACK)
+        arcade.draw_triangle_filled(390 + x, 65 + y, 410 + x, 65 + y, 400 + x, 80 + y, arcade.color.BLACK)
+        arcade.draw_lrtb_rectangle_filled(399 + x, 401 + x, 70 + y, 50 + y, arcade.color.BLACK)
 
     def on_key_press(self, key, modifiers):
         if key == arcade.key.LEFT:
@@ -97,30 +113,25 @@ class MyGame(arcade.Window):
         elif key == arcade.key.DOWN:
             self.player1.change_y = -MOVEMENT_SPEED
 
+        if key == arcade.key.A:
+            self.player2.change_x = -MOVEMENT_SPEED
+        elif key == arcade.key.D:
+            self.player2.change_x = MOVEMENT_SPEED
+        elif key == arcade.key.W:
+            self.player2.change_y = MOVEMENT_SPEED
+        elif key == arcade.key.S:
+            self.player2.change_y = -MOVEMENT_SPEED
+
     def on_key_release(self, key, modifiers):
         if key == arcade.key.LEFT or key == arcade.key.RIGHT:
-            self.player1.change_x = DRIFT_SPEED
+            self.player1.change_x = 0
         elif key == arcade.key.UP or key == arcade.key.DOWN:
-            self.player1.change_y = DRIFT_SPEED
+            self.player1.change_y = 0
 
-        def draw_sun():
-            # draw the sun
-            arcade.draw_circle_filled(400, 165, 150, arcade.color.SUNRAY)
-
-        def draw_sea():
-            # draw the sea
-            arcade.draw_lrtb_rectangle_filled(0, 800, 70, 0, arcade.color.HONOLULU_BLUE)
-
-        def draw_sailboat(x, y):
-            # draw a sailboat
-            arcade.draw_lrtb_rectangle_filled(380 + x, 420 + x, 60 + y, 50 + y, arcade.color.BLACK)
-            arcade.draw_triangle_filled(390 + x, 65 + y, 410 + x, 65 + y, 400 + x, 80 + y, arcade.color.BLACK)
-            arcade.draw_lrtb_rectangle_filled(399 + x, 401 + x, 70 + y, 50 + y, arcade.color.BLACK)
-
-        draw_sun()
-        draw_sea()
-        draw_sailboat(150, 21)
-        arcade.set_background_color(arcade.color.BURNT_ORANGE)
+        if key == arcade.key.A or key == arcade.key.D:
+            self.player2.change_x = 0
+        elif key == arcade.key.W or key == arcade.key.S:
+            self.player2.change_y = 0
 
 
 def main():
