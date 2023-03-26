b296319611e53a4e32c3903cdeab05e90dfd70b2
Kdub <klw2198@email.vccs.edu>
2023-03-25 20:14:11 -0400

    finished lab 8

--- Lab 08 - Sprites/lab_8.py
+++ Lab 08 - Sprites/lab_8.py
@@ -8,18 +8,38 @@ import arcade
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 GAME_TITLE = "Falling Bombs"
-VOLUME = .5
 CHARACTER_SCALING = .15
-DRIFT_SPEED = -.5
 MOVEMENT_SPEED = 2.5
 BOMB_COUNT = 35
-ENEMY_COUNT = 10
+ENEMY_COUNT = 12
 ENEMY_SPRITE_SCALE = .075
 SPRITE_SCALE_BOMBS = .0375
 
 
 def load_texture_pair(filename):
     return [arcade.load_texture(filename), arcade.load_texture(filename, flipped_horizontally=True)]
+
+
+class Player(arcade.Sprite):
+    def __init__(self):
+        # Set up parent class
+        super().__init__()
+
+    def update(self):
+        """ Move the player """
+        self.center_x += self.change_x
+        self.center_y += self.change_y
+
+        # Check for out-of-bounds
+        if self.left < 0:
+            self.left = 0
+        elif self.right > SCREEN_WIDTH - 10:
+            self.right = SCREEN_WIDTH - 10
+
+        if self.bottom < 0:
+            self.bottom = 0
+        elif self.top > SCREEN_HEIGHT - 10:
+            self.top = SCREEN_HEIGHT - 10
 
 
 class Bomb(arcade.Sprite):
@@ -36,12 +56,13 @@ class Bomb(arcade.Sprite):
 
 class Enemy(arcade.Sprite):
     def update(self):
-        self.center_x += -.5
+        self.center_x += -1.675
 
         # Reset planes that exit the screen
         if self.left < 0:
             self.center_x = random.randrange(SCREEN_WIDTH + 20, SCREEN_WIDTH + 60)
             self.center_y = random.randrange(100, SCREEN_HEIGHT)
+
 
 class MyGame(arcade.Window):
 
@@ -59,6 +80,26 @@ class MyGame(arcade.Window):
         # player info
         self.player_sprite = None
         self.score = 0
+
+        self.set_mouse_visible(False)
+
+        # Sounds
+        self.engine_noise = arcade.load_sound("prop-plane-14513.mp3")
+        arcade.play_sound(self.engine_noise)
+
+        self.explosion = arcade.load_sound("big-impact-7054.mp3")
+
+        self.bomb_collected = arcade.load_sound("reload-123781.mp3")
+
+        # Track the current state of what key is pressed
+
+        self.left_pressed = False
+
+        self.right_pressed = False
+
+        self.up_pressed = False
+
+        self.down_pressed = False
 
     def setup(self):
         """ Set up the game and initialize the variables. """
@@ -136,21 +177,65 @@ class MyGame(arcade.Window):
         arcade.draw_triangle_filled(390 + x, 65 + y, 410 + x, 65 + y, 400 + x, 80 + y, arcade.color.BLACK)
         arcade.draw_lrtb_rectangle_filled(399 + x, 401 + x, 70 + y, 50 + y, arcade.color.BLACK)
 
-    def on_key_press(self, key, modifiers):
-        if key == arcade.key.A:
-            self.player_sprite.change_x = -MOVEMENT_SPEED
-        elif key == arcade.key.D:
-            self.player_sprite.change_x = MOVEMENT_SPEED
-        elif key == arcade.key.W:
+    def update_player_speed(self):
+        # Calculate speed based on the keys pressed
+        self.player_sprite.change_x = 0
+        self.player_sprite.change_y = 0
+
+        if self.up_pressed and not self.down_pressed:
             self.player_sprite.change_y = MOVEMENT_SPEED
-        elif key == arcade.key.S:
+
+        elif self.down_pressed and not self.up_pressed:
             self.player_sprite.change_y = -MOVEMENT_SPEED
 
+        if self.left_pressed and not self.right_pressed:
+            self.player_sprite.change_x = -MOVEMENT_SPEED
+
+        elif self.right_pressed and not self.left_pressed:
+            self.player_sprite.change_x = MOVEMENT_SPEED
+
+    def on_update(self, delta_time):
+        self.player_list.update()
+
+    def on_key_press(self, key, modifiers):
+
+        """Called whenever a key is pressed."""
+
+        if key == arcade.key.UP:
+            self.up_pressed = True
+            self.update_player_speed()
+
+        elif key == arcade.key.DOWN:
+            self.down_pressed = True
+            self.update_player_speed()
+
+        elif key == arcade.key.LEFT:
+            self.left_pressed = True
+            self.update_player_speed()
+
+        elif key == arcade.key.RIGHT:
+            self.right_pressed = True
+            self.update_player_speed()
+
     def on_key_release(self, key, modifiers):
-        if key == arcade.key.A or key == arcade.key.D:
-            self.player_sprite.change_x = 0
-        elif key == arcade.key.W or key == arcade.key.S:
-            self.player_sprite.change_y = DRIFT_SPEED
+
+        """Called when the user releases a key. """
+
+        if key == arcade.key.UP:
+            self.up_pressed = False
+            self.update_player_speed()
+
+        elif key == arcade.key.DOWN:
+            self.down_pressed = False
+            self.update_player_speed()
+
+        elif key == arcade.key.LEFT:
+            self.left_pressed = False
+            self.update_player_speed()
+
+        elif key == arcade.key.RIGHT:
+            self.right_pressed = False
+            self.update_player_speed()
 
     def update(self, delta_time):
         """ Movement and game logic """
@@ -161,21 +246,18 @@ class MyGame(arcade.Window):
 
         # list of all sprites that collide with the player.
         bomb_hit_list = arcade.check_for_collision_with_list(self.player_sprite, self.bomb_list)
-
         # Loop through each colliding sprite, remove it, and add to the score.
         for bomb in bomb_hit_list:
             bomb.remove_from_sprite_lists()
+            arcade.play_sound(self.bomb_collected)
             self.score += 1
 
-    def reset_pos(self):
         # Reset enemy planes after collision
         enemy_hit_list = arcade.check_for_collision_with_list(self.player_sprite, self.enemy_list)
-
         for enemy in enemy_hit_list:
-            if enemy_hit_list == True:
-                enemy.remove_from_sprite_lists()
-                self.reset_pos()
-                self.score -= 1
+            enemy.remove_from_sprite_lists()
+            arcade.play_sound(self.explosion)
+            self.score -= 3
 
 
 def main():
