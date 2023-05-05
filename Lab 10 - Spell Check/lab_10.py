31b27fcf1deeb2f42fd74adf69892649b89a1a28
Kdub <klw2198@email.vccs.edu>
2023-05-04 23:26:47 -0400

    lab 10

--- Lab 10 - Spell Check/lab_10.py
+++ Lab 10 - Spell Check/lab_10.py
@@ -18,44 +18,42 @@ def read_in_file(file_name):
 
     dictionary_file.close()
 
-    print("---Linear Search---")
-
     alice_file = open("AliceInWonderLand200.txt")
+    alice_text = []
 
     for each_line in alice_file:
-        words = []
-        each_line = split_line(each_line)
-        words.append(each_line)
+        alice_text.append(each_line)
+        #words = []
+        #each_line = split_line(each_line)
+        #words.append(each_line)
 
-        for word in dictionary_list:
-            line_number = 0
-            line_number += 1
-            if not word != dictionary_list:
-                print("line", line_number, "has misspelled word", word)
+        #for word in dictionary_list:
+           # line_number = 0
+            #line_number += 1
+            #if not word != dictionary_list:
+             #   print("line", line_number, "has misspelled word", word)
 
     alice_file.close()
 
-    return dictionary_list
+    return dictionary_list, alice_file
 
 
-def linear_search(key, word_list):
+def linear_search(dictionary_list, word_list):
+    print("---Linear Search---")
+
     list_pos = 0
-
-    while list_pos < len(word_list) and word_list[list_pos] != key:
+    while list_pos < len(word_list) and word_list[list_pos] != dictionary_list:
         list_pos += 1
 
     if list_pos < len(word_list):
-        print("word found at position", list_pos)
+        print("the word is not in the list")
     else:
-        print("not found")
-    return list_pos
+        print("not in list")
 
 
 def main():
-    key = "Alice in Wonderland"
-
-    word_list = read_in_file("AliceInWonderLand200.txt")
-    linear_search(key, word_list)
+    word_list, dictionary_list = read_in_file("AliceInWonderLand200.txt")
+    linear_search(dictionary_list, word_list)
 
 
 main()
